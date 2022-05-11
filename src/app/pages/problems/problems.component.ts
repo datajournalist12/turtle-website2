e8c73fa2780fd812b98c8feb46bd8f82a0b6e516
Alex Heeb <alexheeb@Alexs-Mac-mini.lan>
2022-05-10 21:56:36 -0500

    fixed ellie narrative and added problem links

--- src/app/pages/problems/problems.component.ts
+++ src/app/pages/problems/problems.component.ts
@@ -14,15 +14,15 @@ export class ProblemsComponent implements OnInit {
 
 
   data1 = [
-    {header: "ANIMAL NEGLECT", subhead: "Turtle races where entertainment is prioritized over animal welfare lead to neglect and abuse", image: "./../../../assets/svg/flipped-turtle.png", width: "75%"},
-    {header: "DISEASE", subhead: "Turtle race conditions cause disease and risk spreading it back to wild populations", image: "./../../../assets/svg/disease-turtle-2.png", width: "35%"},
-    {header: "RELOCATION", subhead: "Relocating highly territorial turtles into new areas results in devastating mortality", image: "./../../../assets/svg/hill.png", width: "50%"},
+    {header: "ANIMAL NEGLECT", subhead: "Turtle races where entertainment is prioritized over animal welfare lead to neglect and abuse", image: "./../../../assets/svg/flipped-turtle.png", width: "75%", path: "/neglect"},
+    {header: "DISEASE", subhead: "Turtle race conditions cause disease and risk spreading it back to wild populations", image: "./../../../assets/svg/disease-turtle-2.png", width: "35%",  path: "/disease"},
+    {header: "RELOCATION", subhead: "Relocating highly territorial turtles into new areas results in devastating mortality", image: "./../../../assets/svg/hill.png", width: "50%",  path: "/relocation"},
    ]
 
    data2 = [
-    {header: "THERMAL ISSUES", subhead: "Turtles can overheat when raced across hot pavement or when left in the sun", image: "./../../../assets/svg/hot-turtle.png", width: "55%"},
-    {header: "SPECIES DECLINES", subhead: "Turtle populations quickly hit the tipping point toward decline when overcollected", image: "./../../../assets/svg/recession.png", width: "45%"},
-    {header: "ENDANGERED SPECIES", subhead: "Turtle races have been caught repeatedly using endangered and protected turtle species", image: "./../../../assets/svg/endangered.png", width: "50%"}
+    {header: "THERMAL ISSUES", subhead: "Turtles can overheat when raced across hot pavement or when left in the sun", image: "./../../../assets/svg/hot-turtle.png", width: "55%",  path: "/hot"},
+    {header: "SPECIES DECLINES", subhead: "Turtle populations quickly hit the tipping point toward decline when overcollected", image: "./../../../assets/svg/recession.png", width: "45%",  path: "/populations"},
+    {header: "ENDANGERED SPECIES", subhead: "Turtle races have been caught repeatedly using endangered and protected turtle species", image: "./../../../assets/svg/endangered.png", width: "50%",  path: "/endangered"}
    ]
 
 }
